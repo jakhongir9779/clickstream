95b35accd17182aa8678023637c2be929a6e71a8
j.madaminov <j.madaminov@uzum.com>
2024-04-16 15:23:05 +0500

    simplified AppLifecycleEvent

--- clickstream/src/androidMain/kotlin/analytics/sdk/clickstream/lifecycle/AndroidAppLifecycleObserver.kt
+++ clickstream/src/androidMain/kotlin/analytics/sdk/clickstream/lifecycle/AndroidAppLifecycleObserver.kt
@@ -9,13 +9,10 @@ class AndroidAppLifecycleObserver(val onEvent: (AppLifecycleEvent) -> Unit) :
     LifecycleEventObserver {
     override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
         when (event) {
-            Lifecycle.Event.ON_CREATE -> onEvent(AppLifecycleEvent.ON_CREATE)
-            Lifecycle.Event.ON_START -> onEvent(AppLifecycleEvent.ON_START)
-            Lifecycle.Event.ON_RESUME -> onEvent(AppLifecycleEvent.ON_RESUME)
-            Lifecycle.Event.ON_PAUSE -> onEvent(AppLifecycleEvent.ON_PAUSE)
-            Lifecycle.Event.ON_STOP -> onEvent(AppLifecycleEvent.ON_STOP)
-            Lifecycle.Event.ON_DESTROY -> onEvent(AppLifecycleEvent.ON_DESTROY)
-            Lifecycle.Event.ON_ANY -> {}
+            Lifecycle.Event.ON_START -> onEvent(AppLifecycleEvent.STARTED)
+            Lifecycle.Event.ON_STOP -> onEvent(AppLifecycleEvent.STOPPED)
+            Lifecycle.Event.ON_RESUME -> onEvent(AppLifecycleEvent.RESUMED)
+            else -> {}
         }
     }
 }
--- clickstream/src/commonMain/kotlin/analytics/sdk/clickstream/lifecycle/AppLifecycleEvent.kt
+++ clickstream/src/commonMain/kotlin/analytics/sdk/clickstream/lifecycle/AppLifecycleEvent.kt
@@ -1,5 +1,5 @@
 package analytics.sdk.clickstream.lifecycle
 
 enum class AppLifecycleEvent {
-    ON_CREATE, ON_START, ON_PAUSE, ON_RESUME, ON_STOP, ON_DESTROY
+    STARTED, RESUMED, STOPPED
 }
