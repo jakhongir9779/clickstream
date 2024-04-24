ce4c683a5e36446a2ddd54061b45a238a12c549c
j.madaminov <j.madaminov@uzum.com>
2024-04-24 18:14:14 +0500

    Release new version 0.0 clickstream

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://nexus.infra.cluster.daymarket.uz/repository/market-mobile//uz/uzum/mobile/analytics/sdk/clickstream-kmmbridge/1.6/clickstream-kmmbridge-1.6.zip"
-let remoteKotlinChecksum = "2fc7b1ef01f910a2beb2bb9291749d0ef07e7769d633a8ec4b43b198495dde24"
+let remoteKotlinUrl = "https://nexus.infra.cluster.daymarket.uz/repository/market-mobile//uz/uzum/mobile/analytics/sdk/clickstream-kmmbridge/0.0/clickstream-kmmbridge-0.0.zip"
+let remoteKotlinChecksum = "6b646bfd0282b509cd42bdc7aaa8ed3caba4083b8c2e67f745ace03fef4d1972"
 let packageName = "clickstream"
 // END KMMBRIDGE BLOCK
 
