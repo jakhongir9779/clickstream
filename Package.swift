ac172e0fc0553d7a5a59898b7be64a1d16c3a7ba
j.madaminov <j.madaminov@uzum.com>
2024-04-26 13:38:13 +0500

    fixing package

--- Package.swift
+++ Package.swift
@@ -1,28 +1,21 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://nexus.infra.cluster.daymarket.uz/repository/market-mobile/uz/uzum/mobile/analytics/sdk/clickstream-kmmbridge/0.0/clickstream-kmmbridge-0.0.zip"
-let remoteKotlinChecksum = "6b646bfd0282b509cd42bdc7aaa8ed3caba4083b8c2e67f745ace03fef4d1972"
-let packageName = "clickstream"
-// END KMMBRIDGE BLOCK
-
 let package = Package(
-    name: packageName,
+    name: "clickstream",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         .library(
-            name: packageName,
-            targets: [packageName]
+            name: "clickstream",
+            targets: ["clickstream"]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: packageName,
-            url: remoteKotlinUrl,
-            checksum: remoteKotlinChecksum
-        )
+            name: "clickstream",
+            path: "./clickstream.xcframework"
+        ),
     ]
 )
