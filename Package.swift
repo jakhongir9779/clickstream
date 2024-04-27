cc626ec50eb0743db69963b8341a6f29ff28dbdf
runner <runner@Mac-1714202051034.local>
2024-04-27 07:49:49 +0000

    KMM SPM package release for 0.1.2

--- Package.swift
+++ Package.swift
@@ -1,7 +1,11 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/jakhongir9779/clickstream/uz/uzum/mobile/analytics/sdk/clickstream-kmmbridge/0.0/clickstream-kmmbridge-0.0.zip"
+let remoteKotlinChecksum = "b90afbd187c1ced2cb4fb52db5f067f5ff5d71904f5decf2a43414b89173411f"
 let packageName = "clickstream"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
@@ -17,7 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./clickstream/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
